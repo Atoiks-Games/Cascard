436242771cde43dd6ad56622d1fabe7a50b30c29
Paul Teng <plankp@outlook.com>
2018-05-09 05:50:43 -0400

    Player can abort game part way in GUI mode

--- src/main/java/org/atoiks/games/cascard/environment/SwingGame.java
+++ src/main/java/org/atoiks/games/cascard/environment/SwingGame.java
@@ -22,11 +22,17 @@ public class SwingGame extends Game {
         int inp = -1;
         while (inp < 2) {
             try {
-                inp = Integer.parseInt(JOptionPane.showInputDialog(
+                final String raw = JOptionPane.showInputDialog(
                         null,
                         "How many players are playing? (minimum of 2)",
-                        null,
-                        JOptionPane.QUESTION_MESSAGE));
+                        "Game Configuration",
+                        JOptionPane.QUESTION_MESSAGE);
+
+                if (raw == null) {
+                    // Player hit the exit button, quit
+                    return false;
+                }
+                inp = Integer.parseInt(raw);
             } catch (NumberFormatException ex) {
                 // restart the loop, ask for input again
             }
@@ -43,13 +49,23 @@ public class SwingGame extends Game {
 
     @Override
     protected Card pickCard(final int id, final Hand player) {
-        JOptionPane.showMessageDialog(null, "Player " + (id + 1) + "'s turn", null, JOptionPane.INFORMATION_MESSAGE);
+        final int opt = JOptionPane.showOptionDialog(
+                null,
+                "Player " + (id + 1) + ": Continue Game?",
+                "Ready",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, null, null);
+        if (opt == JOptionPane.NO_OPTION) {
+            // Player gives up, end game
+            return null;
+        }
+
         final Vector<String> vector = new Vector<>();
         player.forEachCard(card -> vector.add(Integer.toString(card.id)));
         final JList<String> list = new JList<>(vector);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         final JPanel panel = new JPanel(new BorderLayout());
-        panel.add(new JLabel("Top card is " + topCard.id + "\n\nPlease chose a card from below:"), BorderLayout.NORTH);
+        panel.add(new JLabel("Top card is " + topCard.id + ". Please chose a card from below:"), BorderLayout.NORTH);
         panel.add(list, BorderLayout.CENTER);
 
         int[] indicies = {};
@@ -62,10 +78,9 @@ public class SwingGame extends Game {
     }
 
     @Override
-    protected void broadcastWinner(int id, Hand hand) {
-        JOptionPane.showMessageDialog(null, "Player " + (id + 1) + " won!", null, JOptionPane.WARNING_MESSAGE);
+    protected void broadcastWinner(final int id, final Hand player) {
+        JOptionPane.showMessageDialog(null, "Player " + (id + 1) + " won!", "GG", JOptionPane.WARNING_MESSAGE);
     }
-
 
     @Override
     public void close() {
